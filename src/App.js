9ab2bbc949971930da6c7d60a9e345979dd9a1b0
Serhii-Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2021-05-17 18:21:35 +0100

    world 3D

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Canvas } from "react-three-fiber";
+import { OrbitControls, Stars, Cloud, RoundedBox } from '@react-three/drei';
+import { Physics, useBox, usePlane } from '@react-three/cannon'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Text from './components/Text'
+import Manual from './components/Manual'
+
+function Box() {
+
+    const [ref, api] = useBox(() => {
+        return {
+            mass: 1,
+            position: [0, 10, 0]
+        }
+    })
+
+
+    return (
+
+        <mesh onClick = { () => {
+                      
+             api.velocity.set(0.1,5,0.1)
+         } }  ref = {ref} position = { [0,10,0]}  >
+            <boxBufferGeometry attach ="geometry"  />
+             <meshLambertMaterial attach="material" color="gold"  />                         
+
+         </mesh>
+
+    )
 }
 
-export default App;
+
+function Plane(props) {
+
+    const [ref, api] = usePlane(() => {
+
+        return {
+            rotation: [-Math.PI / 2, 0, 0]
+        }
+    })
+
+
+    return (
+        <mesh  className='plane'  rotation = {[-Math.PI/2, 0, 0 ]} >
+            <planeBufferGeometry attach ="geometry" args = {[15,15]} />
+             <meshLambertMaterial attach="material" color="blue"   />                         
+         
+         </mesh>
+
+    )
+}
+
+
+export default function App() {
+
+
+    useEffect(() => {
+        let z = document.querySelector('.can')
+
+        z.style.border = 'dashed'
+        setTimeout(() => {
+
+            z.style.border = 'none'
+
+        }, 12000)
+
+    })
+
+
+
+    return (
+
+        <div>
+            <Text />
+            <Manual  /> 
+            <Canvas className ='can'  >
+       
+            <OrbitControls   />
+            
+              <Stars />
+           
+            <ambientLight  intensity = {0.5}  />
+ 
+            <spotLight
+              position = {[10,15,10]}  angle={0.9}/>
+
+         
+              <Physics>
+               <Cloud position = {[7,12,0]} />
+                <Box   />
+                <Plane  />
+             </Physics>
+          </Canvas>
+
+        </div>
+    )
+}
---
+++ src/components/Text.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function Text() {
+	return (
+         <div>
+             <h2>crated by Serhii Bondarchuk!</h2> 
+             <h4><em> amax9111@gmail.com </em></h4>
+  
+
+          </div>
+
+		)
+}
+
+export default Text
